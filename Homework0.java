3e7f4230fad9a0e392ab482ca271115c9117db48
blakemasse <56727507+blakemasse@users.noreply.github.com>
2024-01-26 11:15:04 -0500

    Create Homework0.java

---
+++ Homework0.java
@@ -0,0 +1,9 @@
+
+
+public class Homework0 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Blake Masse");
+                
+    }
+}
